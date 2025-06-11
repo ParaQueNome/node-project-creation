70107eee90c29f58f15257233686c1fd286b4d1b
guilhermeaa1 <guilhermeaa@ciandt.com>
2025-06-11 10:54:28 -0300

    feat: initializing projetc: - instance of express server; - configuration for the tsconfig file; - desing pattern MVC structured;

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { Request, Response } from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/health', (req : Request, res : Response) => {
+    res.status(200).json({
+        status: 'UP',
+        timestamp: new Date().toISOString()
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
