0a27375b28b1c4f2cd22de571d4658445a445991
guilhermeaa1 <guilhermeaa@ciandt.com>
2025-06-23 15:18:15 -0300

    feat: endpoint for listing skills: \n - `skills.ts`: schema model for table skill in database; \n - `skillsRepository.ts`: persistence function that calls select method from table skills and list all skills; \n - `skillsController.ts`: controller containing the function for calling listAllSkills enpoint; \n - `routes.ts`: adding an endpoint called listSkills;

---
+++ dist/models/skills.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const skillsSchema = (data) => {
+    return {
+        id: data.id,
+        title: data.title,
+        rate: data.rate
+    };
+};
+exports.default = skillsSchema;
+//# sourceMappingURL=skills.js.map
---
+++ src/models/skills.ts
@@ -0,0 +1,11 @@
+
+
+const skillsSchema = (data: {id: number, title: string, rate: number }) => { 
+    return { 
+        id: data.id,
+        title: data.title,
+        rate: data.rate
+    }
+}
+
+export default skillsSchema;
